22a6ffb775effb017eb7106e60bb5f355a83b769
gfoza <gfozza@hotmail.com>
2020-08-05 09:52:26 -0300

    Adição de Middlewares

---
+++ src/index.js
@@ -0,0 +1,109 @@
+const express =  require("express");
+const { uuid, isUuid } = require("uuidv4");
+const { request } = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const scraps = [];
+
+function logRequests(request, response, next) {
+    console.time("tempo");
+
+        const { method, url } = request;
+
+            const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+        next();
+
+        console.log(logLabel);
+    console.timeEnd("tempo");
+  }
+
+function validaId(request, response, next) {
+    const { id } = request.params;
+
+    if(!isUuid(id)) {
+        return response
+            .status(400)
+            .json({ error: "Param sent is not a valid UUID" });
+    }
+    next();
+}
+
+function validateTitleAndMessage(request, response, next) {
+    const { title , message } = request.body;
+  
+    if (title == "" || message == ""){
+      return response.status(400).json({ error: "Param sent is not a valid Title" });
+    }
+    
+    next();
+  }
+
+app.use(logRequests);
+app.use(validateTitleAndMessage);
+app.use("/projects/:id", validaId);
+
+app.get('/projects', (request, response) => { //Busca os scraps inseridos
+    const { title } = request.query;
+
+    const results = title
+        ? scraps.filter(scrap => 
+            scrap.title.toLowerCase().includes(title.toLowerCase()))
+        :   scraps;
+        
+    return response.json(results);
+});
+
+app.post('/projects', (request, response) => { //Insere scraps
+    const { title, message } = request.body;
+
+    const scrap = { id: uuid(), title, message };
+
+    scraps.push(scrap)
+
+    return response.json(scrap);
+});
+
+app.put('/projects/:id', (request, response) => { //Edita scraps
+    const { id } = request.params;
+    const { title, message } = request.body;
+
+    const scrapIndex = scraps.findIndex(scrap => scrap.id == id);
+
+    if(scrapIndex < 0) {
+        return response.status(400).json({ error: "Scrap not found" });
+    }
+
+    const scrap = {
+        id, 
+        title, 
+        message
+    };
+
+    scraps[scrapIndex] = scrap;
+
+    return response.json(scrap);
+}); 
+
+app.delete('/projects/:id', (request, response) => {// Deleta scraps
+    const { id } = request.params;
+    const { title, message } = request.params;
+
+    const scrapIndex = scraps.findIndex(scrap => scrap.id == id);
+
+    if(scrapIndex < 0) {
+        return response.status(400).json({ error: "Scrap not found" });
+    }
+
+    scraps.splice(scrapIndex, 1);
+
+    return response.status(204).send(); //código de sucesso com resposta vazia
+});
+
+const port = 3333;
+app.listen(port, () => {
+    console.log(`Server up nd running on PORT ${port}`)
+});
